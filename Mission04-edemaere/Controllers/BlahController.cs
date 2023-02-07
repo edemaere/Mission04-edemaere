38849dc73ce5b7d509521aab7aeabd0b3c5e8c2f
Ezra DeMaere <demaere.ezra@gmail.com>
2023-02-06 21:41:12 -0700

    convert e'erthang to MVC

--- Mission04-edemaere/Controllers/BlahController.cs
+++ Mission04-edemaere/Controllers/BlahController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mission04_edemaere.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,56 @@ namespace Mission04_edemaere.Controllers
         {
 
             return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View("Calculator", new Calculator());
+        }
+
+
+        [HttpPost]
+        public IActionResult Calculator(Calculator model)
+        {
+
+            // Calculate correct grade weight based on syllabus
+            float assignments = model.Assignments * 0.5f;
+            float grpProject = model.GrpProject * 0.1f;
+            float quizzes = model.Quizzes * 0.1f;
+            float midterm = model.Midterm * 0.1f;
+            float final = model.Final * 0.1f;
+            float intex = model.Intex * 0.1f;
+
+            //Calculate overall percentage by summing percentages
+            model.overallPercent = (assignments +grpProject + quizzes + midterm + final + intex);
+
+            // If the total percent is from 0 - 100
+            if ((model.overallPercent >= 0) && (model.overallPercent <= 100))
+            {
+
+                // Calculate letter grade based on overall percent
+                if (model.overallPercent < 60) { model.letterGrade = "E"; }
+                else if (model.overallPercent < 64) { model.letterGrade = "D-"; }
+                else if (model.overallPercent < 67) { model.letterGrade = "D"; }
+                else if (model.overallPercent < 70) { model.letterGrade = "D+"; }
+                else if (model.overallPercent < 74) { model.letterGrade = "C-"; }
+                else if (model.overallPercent < 77) { model.letterGrade = "C"; }
+                else if (model.overallPercent < 80) { model.letterGrade = "C+"; }
+                else if (model.overallPercent < 84) { model.letterGrade = "B-"; }
+                else if (model.overallPercent < 87) { model.letterGrade = "B"; }
+                else if (model.overallPercent < 90) { model.letterGrade = "B+"; }
+                else if (model.overallPercent < 94) { model.letterGrade = "A-"; }
+                else { model.letterGrade = "A"; }
+            }
+            else
+            {
+                model.overallPercent = 0f;
+                model.letterGrade = "E";
+            }
+
+            return View(model);
         }
     }
 }
---
+++ Mission04-edemaere/Models/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_edemaere.Models
+{
+    public class Calculator
+    {
+
+        // Validation in the model to ensure input between 0 and 100
+        [Range(0,100)]
+        public float Assignments { get; set; }
+        [Range(0, 100)]
+        public float GrpProject { get; set; }
+        [Range(0, 100)]
+        public float Quizzes { get; set; }
+        [Range(0, 100)]
+        public float Midterm { get; set; }
+        [Range(0, 100)]
+        public float Final { get; set; }
+        [Range(0, 100)]
+        public float Intex { get; set; }
+
+        // Set defaults for calculated variables. When user submits, these will be updated
+        public float overallPercent { get; set; } = 0f;
+        public string letterGrade { get; set; } = "E";
+
+    }
+
+}
